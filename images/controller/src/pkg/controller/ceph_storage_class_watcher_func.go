c9527baba848d97a88c829d5d580ffd8663ad7c8
Nikolay Demchuk <nikolay.demchuk@flant.com>
2024-07-12 01:04:12 +1000

    typo fix Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

--- images/controller/src/pkg/controller/ceph_storage_class_watcher_func.go
+++ images/controller/src/pkg/controller/ceph_storage_class_watcher_func.go
@@ -302,7 +302,7 @@ func GetStoragecClassParams(cephSC *storagev1alpha1.CephStorageClass, controller
 	}
 
 	if cephSC.Spec.Type == storagev1alpha1.CephStorageClassTypeRBD {
-		params["imageFeatures"] = "layering,journalingexclusive-lock,object-map,fast-diff"
+		params["imageFeatures"] = "layering,journaling,exclusive-lock,object-map,fast-diff"
 		params["csi.storage.k8s.io/fstype"] = cephSC.Spec.RBD.DefaultFSType
 		params["pool"] = cephSC.Spec.RBD.Pool
 		params["mounter"] = "rbd-nbd"
