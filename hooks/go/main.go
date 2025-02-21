5563253f5b1e24f6a3e33d243add6dd8332e7064
v.oleynikov <vasily.oleynikov@flant.com>
2025-02-21 07:39:18 +0300

    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- hooks/go/main.go
+++ hooks/go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/deckhouse/module-sdk/pkg/app"
 	// подключаем все нужные/созданные хуки (package-ы) здесь
+
+	_ "csi-ceph/050-migrate-auth-to-connection"
 )
 
 func main() {
