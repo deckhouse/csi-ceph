2896c63474634060be6b6ef94ae80b15b48a0043
v.oleynikov <vasily.oleynikov@flant.com>
2025-02-21 07:20:07 +0300

    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- hooks/go/050-migrate-auth-to-connection/migrate-auth-to-connection.go
+++ hooks/go/050-migrate-auth-to-connection/migrate-auth-to-connection.go
@@ -1,11 +1,23 @@
 package hooks_common
 
 import (
+	"context"
 	"fmt"
 
 	consts "csi-ceph/consts"
+
+	"github.com/deckhouse/module-sdk/pkg"
+	"github.com/deckhouse/module-sdk/pkg/registry"
 )
 
-var _ = func() {
+var _ = registry.RegisterFunc(configMigrateAuthToConnection, handlerMigrateAuthToConnection)
+
+var configMigrateAuthToConnection = &pkg.HookConfig{
+	OnBeforeHelm: &pkg.OrderedConfig{Order: 5},
+}
+
+func handlerMigrateAuthToConnection(_ context.Context, input *pkg.HookInput) error {
 	fmt.Printf("Module %s Hook test", consts.MODULE_NAME)
+
+	return nil
 }
