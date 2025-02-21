8730bdce7adb7602a35b915463c1f2a4b660e325
v.oleynikov <vasily.oleynikov@flant.com>
2025-02-21 08:46:28 +0300

    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- hooks/go/050-migrate-auth-to-connection/migrate-auth-to-connection.go
+++ hooks/go/050-migrate-auth-to-connection/migrate-auth-to-connection.go
@@ -3,6 +3,7 @@ package hooks_common
 import (
 	"context"
 	"fmt"
+	"os"
 
 	consts "csi-ceph/consts"
 
@@ -10,8 +11,19 @@ import (
 	"github.com/deckhouse/module-sdk/pkg/registry"
 
 	"github.com/deckhouse/csi-ceph/api/v1alpha1"
+
+	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	sv1 "k8s.io/api/storage/v1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var _ = registry.RegisterFunc(configMigrateAuthToConnection, handlerMigrateAuthToConnection)
@@ -20,28 +32,65 @@ var configMigrateAuthToConnection = &pkg.HookConfig{
 	OnBeforeHelm: &pkg.OrderedConfig{Order: 5},
 }
 
+func NewKubeClient(kubeconfigPath string) (client.Client, error) {
+	var config *rest.Config
+	var err error
+
+	if kubeconfigPath == "" {
+		kubeconfigPath = os.Getenv("kubeconfig")
+	}
+
+	controllerruntime.SetLogger(logr.New(ctrllog.NullLogSink{}))
+
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		resourcesSchemeFuncs = []func(*apiruntime.Scheme) error{
+			v1alpha1.AddToScheme,
+			clientgoscheme.AddToScheme,
+			extv1.AddToScheme,
+			v1.AddToScheme,
+			sv1.AddToScheme,
+		}
+	)
+
+	scheme := apiruntime.NewScheme()
+	for _, f := range resourcesSchemeFuncs {
+		err = f(scheme)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	clientOpts := client.Options{
+		Scheme: scheme,
+	}
+
+	return client.New(config, clientOpts)
+}
+
 func handlerMigrateAuthToConnection(_ context.Context, input *pkg.HookInput) error {
 	fmt.Printf("Module %s Hook test", consts.MODULE_NAME)
 
-	// Создаем REST-клиент с помощью controller-runtime
-	cfg, err := config.GetConfigWithContext("")
+	ctx := context.Background()
+	cl, err := NewKubeClient("")
 	if err != nil {
-		fmt.Printf("Ошибка загрузки конфигурации Kubernetes: %v\n", err)
-		return err
+		klog.Fatal(err.Error())
 	}
 
-	// Создаем клиент для работы с Kubernetes API
-	cl, err := client.New(cfg, client.Options{})
+	cephClusterAuthenticationList := &v1alpha1.CephClusterAuthenticationList{}
+
+	err = cl.List(ctx, cephClusterAuthenticationList)
 	if err != nil {
-		fmt.Printf("Ошибка создания Kubernetes клиента: %v\n", err)
-		return err
+		klog.Fatal(err)
 	}
 
-	var cephClusterAuthList v1alpha1.CephClusterAuthenticationList
-	err = cl.List(context.TODO(), &cephClusterAuthList)
-	if err != nil {
-		fmt.Printf("Ошибка при запросе CephClusterAuthenticationList: %v\n", err)
-		return nil
+	for _, item := range cephClusterAuthenticationList.Items {
+		fmt.Printf(item.Name)
 	}
 
 	return nil
