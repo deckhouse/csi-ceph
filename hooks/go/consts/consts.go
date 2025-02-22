592d81a1cab4bb6c98d572184d8bb0b06a82c562
v.oleynikov <vasily.oleynikov@flant.com>
2025-02-22 22:38:40 +0300

    Fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- hooks/go/consts/consts.go
+++ hooks/go/consts/consts.go
@@ -1,9 +1,6 @@
 package consts
 
 const (
-	// имя модуля должно быть в camelCase
 	MODULE_NAME      string = "csiCeph"
 	MODULE_NAMESPACE string = "d8-csi-ceph"
-
-	POSTGRES_OPERATOR_WEBHOOKS_CERT_CN string = "webhooks"
 )
--- hooks/go/main.go
+++ hooks/go/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/deckhouse/module-sdk/pkg/app"
-	// подключаем все нужные/созданные хуки (package-ы) здесь
 
 	_ "csi-ceph/050-migrate-auth-to-connection"
 )
