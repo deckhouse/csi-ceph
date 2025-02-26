0d193812a50425f0151a76dbc538e97626b0a264
Aleksandr Zimin <alexandr.zimin@flant.com>
2025-02-26 13:12:06 +0300

    add backups and pv migrate (#35)
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

--- api/v1alpha1/register.go
+++ api/v1alpha1/register.go
@@ -47,6 +47,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&CephClusterConnectionList{},
 		&CephClusterAuthentication{},
 		&CephClusterAuthenticationList{},
+		&CephMetadataBackup{},
+		&CephMetadataBackupList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
