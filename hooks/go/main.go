07f0807e949a5fb175fb1a12332eb0b77dbc64db
v.oleynikov <vasily.oleynikov@flant.com>
2025-03-14 13:03:07 +0300

    Switched to go hook for delete sc and secrets
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- hooks/go/main.go
+++ hooks/go/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/deckhouse/module-sdk/pkg/app"
 
 	_ "github.com/deckhouse/csi-ceph/hooks/go/020-webhook-certs"
+	_ "github.com/deckhouse/csi-ceph/hooks/go/030-remove-sc-and-secrets-on-module-delete"
 	_ "github.com/deckhouse/csi-ceph/hooks/go/050-migrate-auth-to-connection"
 )
 
